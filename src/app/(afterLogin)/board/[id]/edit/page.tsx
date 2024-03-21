b4cf2c3109d88d7bfd7d498e5257a9ee029bf1b5
Yeeun Lee <exsilver305@gmail.com>
2024-03-21 15:55:23 +0900

    Fix: 빌드 오류 해결

--- src/app/(afterLogin)/board/[id]/edit/page.tsx
+++ src/app/(afterLogin)/board/[id]/edit/page.tsx
@@ -2,10 +2,10 @@
 import PostForm from "@/assets/components/Board/PostForm";
 import { useParams } from "next/navigation";
 
-const boardEdit = () => {
+const BoardEdit = () => {
   const { id } = useParams() as { id: string };
 
   return <PostForm postId={id} />;
 };
 
-export default boardEdit;
+export default BoardEdit;
--- src/assets/components/Items/BoradItem/index.tsx
+++ src/assets/components/Items/BoradItem/index.tsx
@@ -15,17 +15,7 @@ const BoardItem = ({ id }: PostProps) => {
     <div className={styles.box} onClick={() => router.push(`board/${id}`)}>
       <p className={styles.title}>새로운 레시피 소개합니다.</p>
 
-      <p className={styles.explain}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <p className={styles.explain}>example</p>
     </div>
   );
 };
