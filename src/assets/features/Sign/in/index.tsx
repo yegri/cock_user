af670a40203104de9cac64ba4ce8779effbece84
Yeeun Lee <exsilver305@gmail.com>
2024-03-23 21:02:58 +0900

    Fix: env에 url 추가

--- src/assets/features/Sign/in/index.tsx
+++ src/assets/features/Sign/in/index.tsx
@@ -33,6 +33,8 @@ const SignInPage = () => {
       redirect: false,
     });
 
+    console.log(result);
+
     if (result?.error === "CredentialsSignin") {
       alert("로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.");
     }
